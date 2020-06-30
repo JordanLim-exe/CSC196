bf99a884970d4f9d93600a54dcf21a46196f6ae0
JordanLim-exe <JLim@student.neumont.edu>
2020-06-30 12:00:21 -0600

    Followed Class instructions, Now shows array of random lines underneath the previous design.

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,8 +1,13 @@
-// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include "core.h"
+#include "Math/Math.h"
+#include "Math/Vector2.h"
+#include "Math/Random.h"
 #include <iostream>
+
+nc::Vector2 position{ 400, 300 };
+std::vector<nc::Vector2> points;
+
 
 bool Update(float dt)
 {
@@ -13,10 +18,22 @@ void Draw(Core::Graphics& graphics)
 {
 	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
 	graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+
+	for (size_t i = 0; i < 40 - 1; i++) {
+		nc::Vector2 p1 = points[i];
+		nc::Vector2 p2 = points[i + 1];
+
+		graphics.DrawLine(p1.x, p1.y, p2.x, p2.y);
+	}
+
 }
 
 int main()
 {
+	for (size_t i = 0; i < 40; i++) {
+		points.push_back(nc::Vector2{ nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f) });
+	}
+
 	char name[] = "CSC196";
 	Core::Init(name, 800, 600);
 	Core::RegisterUpdateFn(Update);
